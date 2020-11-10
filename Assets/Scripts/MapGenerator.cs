376e5b86b52d7012b43f3cf53fa9a659c2026573
BlaD200 <blad600@gmail.com>
2020-11-10 21:26:01 +0200

    World generation complete. Player added.

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -14,12 +14,18 @@ public class MapGenerator : MonoBehaviour
     public TileBase wampus;
     public TileBase player;
 
+    public GameObject windPrefab;
+    public GameObject stenchPrefab;
+    public GameObject glitterPrefab;
+
     public Tilemap levelMap;
     public Tilemap entitiesMap;
     public Tilemap playerMap;
 
     [Range(4, 10)]
     public int mapSize;
+
+    private List<GameObject> prefabs = new List<GameObject>();
 
 
     // Start is called before the first frame update
@@ -31,7 +37,8 @@ public class MapGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G)) {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
             ClearWorld();
             GenerateWorld();
         }
@@ -52,7 +59,6 @@ public class MapGenerator : MonoBehaviour
 
                 if (Random.value <= 0.2 && !(x == 0 && y == 0))
                 {
-                    // TODO add wind
                     continue;
                 }
 
@@ -62,14 +68,64 @@ public class MapGenerator : MonoBehaviour
             }
         }
 
-        playerMap.SetTile(new Vector3Int(0, 0, (int)playerMap.transform.position.z), player);
+        for (int y = 0; y < mapSize; y++)
+        {
+            for (int x = 0; x < mapSize; x++)
+            {
+                Vector3Int pos = new Vector3Int(x, y, (int)levelMap.transform.position.z);
+                if (levelMap.GetTile(pos) == null)
+                {
+                    AddPrefabs(x, y, windPrefab);
+                }
+            }
+        }
 
-        SetRandomTileForEntity(wampus);
-        SetRandomTileForEntity(gold);
+        // playerMap.SetTile(new Vector3Int(0, 0, (int)playerMap.transform.position.z), player);
+
+        int posX, posY;
+
+        (posX, posY) = SetRandomTileForEntity(wampus);
+        if (posX != -1)
+            AddPrefabs(posX, posY, stenchPrefab);
+
+        (posX, posY) = SetRandomTileForEntity(gold);
+        if (posX != -1)
+        {
+            Vector3 windPos = new Vector3(posX + .5f, posY + 0.5f, (int)playerMap.transform.position.z);
+            prefabs.Add(Instantiate(glitterPrefab, windPos, Quaternion.identity));
+        }
 
     }
 
-    private void SetRandomTileForEntity(TileBase tile)
+    private void AddPrefabs(int x, int y, GameObject prefab)
+    {
+        if (x - 1 >= 0 && y >= 0
+            && !(levelMap.GetTile(new Vector3Int(x - 1, y, (int)levelMap.transform.position.z)) == null))
+        {
+            Vector3 windPos = new Vector3(x - 0.5f, y + .5f, (int)playerMap.transform.position.z);
+            prefabs.Add(Instantiate(prefab, windPos, Quaternion.identity));
+        }
+        if (x >= 0 && y - 1 >= 0
+             && !(levelMap.GetTile(new Vector3Int(x, y - 1, (int)levelMap.transform.position.z)) == null))
+        {
+            Vector3 windPos = new Vector3(x + 0.5f, y - .5f, (int)playerMap.transform.position.z);
+            prefabs.Add(Instantiate(prefab, windPos, Quaternion.identity));
+        }
+        if (x + 1 >= 0 && x + 1 < mapSize && y >= 0
+            && !(levelMap.GetTile(new Vector3Int(x + 1, y, (int)levelMap.transform.position.z)) == null))
+        {
+            Vector3 windPos = new Vector3(x + 1.5f, y + .5f, (int)playerMap.transform.position.z);
+            prefabs.Add(Instantiate(prefab, windPos, Quaternion.identity));
+        }
+        if (x >= 0 && y + 1 >= 0 && y + 1 < mapSize
+            && !(levelMap.GetTile(new Vector3Int(x, y + 1, (int)levelMap.transform.position.z)) == null))
+        {
+            Vector3 windPos = new Vector3(x + 0.5f, y + 1.5f, (int)playerMap.transform.position.z);
+            prefabs.Add(Instantiate(prefab, windPos, Quaternion.identity));
+        }
+    }
+
+    private (int, int) SetRandomTileForEntity(TileBase tile)
     {
         int tries = 100;
         while (--tries > 0)
@@ -83,23 +139,32 @@ public class MapGenerator : MonoBehaviour
 
             Vector3Int levelPos = new Vector3Int(x, y, (int)levelMap.transform.position.z);
             Vector3Int entityPos = new Vector3Int(x, y, (int)entitiesMap.transform.position.z);
- 
+
             // pit or wampus or gold
             TileBase onLevelTile = levelMap.GetTile(levelPos);
             TileBase onEntityTile = entitiesMap.GetTile(entityPos);
-            if (onLevelTile == null || onEntityTile == wampus || onEntityTile == gold) {
-                Debug.Log("Pos: " + entityPos + "; Level: " + onLevelTile + "; Entity: " + onEntityTile);
+            if (onLevelTile == null || onEntityTile == wampus || onEntityTile == gold)
+            {
                 continue;
             }
 
             entitiesMap.SetTile(entityPos, tile);
-            return;
+            return (x, y);
         }
 
         Debug.LogWarning("Cannot generate tile: " + tile);
+        return (-1, -1);
     }
 
-    private void ClearWorld(){
+    private void ClearWorld()
+    {
+        for (int i = 0; i < prefabs.Count; i++)
+        {
+            Destroy(prefabs[i]);
+            // prefabs.RemoveAt(i);
+        }
+        prefabs.RemoveAll(item => true);
+
         for (int y = -1; y < mapSize + 1; y++)
         {
             for (int x = -1; x < mapSize + 1; x++)
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 1;
+
+    private Vector3Int position;
+
+    private void Create()
+    {
+        position = new Vector3Int(0, 0, (int)transform.position.z);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            position += Vector3Int.up;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            position += Vector3Int.down;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            position += Vector3Int.right;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            position += Vector3Int.left;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.name + "; " + other.tag);
+    }
+}
